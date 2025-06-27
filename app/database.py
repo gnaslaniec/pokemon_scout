96a996bfb78be8da3f8f0777ff28a09da55eae5d
Gabriel Naslaniec <gabrieljn95@hotmail.com>
2025-06-27 11:26:21 -0300

    fix: fixed in memory config for tests

--- app/database.py
+++ app/database.py
@@ -1,9 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-Base = declarative_base()
-engine = create_engine("sqlite:///pokemons.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///pokemons.db")
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
 
 
 def init_db():
